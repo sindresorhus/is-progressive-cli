9c24966c18d73aebf971c28da42ae553c297377c
Bjorn Stromberg <bjorn@bjornstar.com>
2024-07-06 22:25:04 +0800

    Require Node.js 18 (#2)

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import test from 'ava';
-import execa from 'execa';
+import {execa} from 'execa';
 
 test('main', async t => {
 	const {stdout} = await execa('./cli.js', ['fixture/progressive.jpg']);
